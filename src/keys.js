323b99f857c311f16bb807f16a8c4bc025789951
Yash <yashpandey2002@gmail.com>
2023-03-14 13:12:32 +0530

    fix url

--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,4 @@
 export const URL = {
-    auth: `${process.env.REACT_APP_BASE_URL}/auth`,
-    task: `${process.env.REACT_APP_BASE_URL}/task`,
+    auth: `${process.env.REACT_APP_BASE_URL}auth`,
+    task: `${process.env.REACT_APP_BASE_URL}task`,
 };
